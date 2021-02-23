19d541701a96e09afad721c43d42d3ab83d48f40
lixuefei15 <lixuefei15@jd.com>
2021-02-23 21:08:14 +0800

    搭建登录页面

--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import 'normalize.css/normalize.css'
+
 import Cookies from 'js-cookie'
 import router from './router'
 
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
+
+// global css
+import './assets/styles/index.scss'
+import './assets/icons' // icon
+
+
 
 Vue.use(Element, {
   size: Cookies.get('size') || 'small' // set element-ui default size
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import { getToken } from '@/utils/auth'
-import { MessageBox, Message } from 'element-ui'
+import { Message } from 'element-ui'
 
 
 // 创建 axios 实例
